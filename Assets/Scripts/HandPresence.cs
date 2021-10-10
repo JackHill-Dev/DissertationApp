428b58a6f3a4705a94dd8709b356cc4c15688d3b
JackP-Dev <38892053+JackP-Dev@users.noreply.github.com>
2021-10-10 15:01:07 +0100

    Initial unity project commit

---
+++ Assets/Scripts/HandPresence.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    [SerializeField] bool showController = false;
+    [SerializeField] List<GameObject> controllerPrefabs;
+    [SerializeField] InputDeviceCharacteristics controllerCharacteristics;
+    [SerializeField] GameObject handModelPrefab;
+    [SerializeField] Animator handAnimator;
+
+    private GameObject spawnedHandModel;
+    private GameObject spawnedControllerModel;
+    private InputDevice targetDevice;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TryInit();
+
+      
+    }
+
+    void TryInit()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+
+        foreach(var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+
+        if(devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+            if(prefab)
+                spawnedControllerModel = Instantiate(prefab, transform);
+            else
+                Debug.LogError("Did not fine corresponding controller model");
+
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
+            handAnimator = spawnedHandModel.GetComponent<Animator>();
+        }
+    }
+
+    void UpdateHandAnimation()
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+            handAnimator.SetFloat("Trigger", 0);
+
+
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+            handAnimator.SetFloat("Grip", 0);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!targetDevice.isValid)
+            TryInit();
+        else
+        {
+            if (showController)
+            {
+                spawnedHandModel.SetActive(false);
+                spawnedControllerModel.SetActive(true);
+            }
+            else
+            {
+                spawnedHandModel.SetActive(true);
+                spawnedControllerModel.SetActive(false);
+                UpdateHandAnimation();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/LocomotionController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.Events;
+
+
+
+public class LocomotionController : MonoBehaviour
+{
+    public XRController rightTeleportRay;
+    public XRController leftTeleportRay;
+    public InputHelpers.Button teleportActivationBtn;
+    public float activationThreshold= 0.1f;
+
+    public XRRayInteractor leftInteractorRay;
+    public XRRayInteractor rightInteractorRay;
+    
+    public bool enableRightTeleport { get; set; } = true;
+    public bool enableLeftTeleport { get; set; } = true;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = new Vector3();
+        Vector3 norm = new Vector3();
+        int index = 0;
+        bool validTarget = false;
+        
+        
+        if(rightTeleportRay)
+        {
+            bool isRightHovering = rightInteractorRay.TryGetHitInfo(out pos, out norm, out index, out validTarget);
+            rightTeleportRay.gameObject.SetActive(enableRightTeleport && CheckIfActivated(rightTeleportRay) && !isRightHovering);
+        }
+        
+        if(leftTeleportRay)
+        {
+            bool isLeftHovering = leftInteractorRay.TryGetHitInfo(out pos, out norm, out index, out validTarget);
+            leftTeleportRay.gameObject.SetActive(enableLeftTeleport && CheckIfActivated(leftTeleportRay) && !isLeftHovering) ;
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationBtn, out bool isActivated, activationThreshold);
+        return isActivated;
+
+    }
+}
